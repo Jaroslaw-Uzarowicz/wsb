476924a2e4d54459adcf1c825d021a121e0d2516
Jaroslaw-Uzarowicz <uzarowicz84.games@gmail.com>
2022-10-05 11:08:23 +0200

    Python

--- Python/project1.py
+++ Python/project1.py
@@ -1,2 +1,57 @@
+'''
 print ("test")
-output ("Eryk hehe")
+print(2)
+x = 10.1234
+print(x)
+print("{:.2f}".format(x))
+
+#Potęgowanie
+pow=2**10
+print(pow)
+
+"""Pobieranie danych
+z klawiatury"""
+
+print("text")
+
+
+x=2
+y=6
+"xor"
+result=x^y
+print(result)
+
+#konkateracja +
+name = input()
+print("Twoje imię: "+name)
+
+length= len(name)
+print(length)
+print()
+FirstLetter=name[0]
+print(FirstLetter)
+print(type(name))
+print(type(x))
+y=10.5
+print(type(y))
+
+input("\nPodaj swoj wiek: " )
+lastletter=name[-1]
+print(lastletter)
+'''
+
+'''
+#konwersja
+x = "5"
+print(type(x))
+x = int(x)
+print(type(x))
+x=x/2
+print(x)
+print(type(x))
+surname="Kowalski"
+print(surname[0]) # K
+print(surname[0:3]) # Kow
+print(surname[-2:]) #
+print(surname[:-2]) #
+print(surname[:-2:2])
