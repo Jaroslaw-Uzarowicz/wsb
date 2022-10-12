45ac8d4400ffdf97f9d8dedf2c4435084262c230
Jaroslaw-Uzarowicz <uzarowicz84.games@gmail.com>
2022-10-12 11:10:12 +0200

    Python

---
+++ Python/3_instrukcje_warunkowe.py
@@ -0,0 +1,30 @@
+'''
+x=input('podaj wartość: ')
+if x=='10':
+    print('Podałeś 10')
+else:
+    print('inna wartość niż 10')
+print(type(x))
+
+y=False
+if y:
+    print('prawda')
+else:
+    print('fałsz')
+    '''
+
+'''
+    Użytkownik podaje wartośći trzech zmiennych: x,y,z.
+    Wyświetl, która z tych trzech wartościjest największa
+    
+'''
+x=input('Podaj wartość x: ')
+y=input('Podaj wartość y: ')
+z=input('Podaj wartość z: ')
+
+if int(x)> int(y) and int(x)>int(z):
+    print('Największa to x = ' + x)
+elif int(y)> int(x) and int(y)>int(z):
+     print('Największa to y = ' + y)
+else:
+    print('Największa to z = ' + z)
