452bd98a86711c837e2c09acf7acb3a8a9656644
Jaroslaw-Uzarowicz <uzarowicz84.games@gmail.com>
2022-10-12 10:25:09 +0200

    Python

--- Python/2_string.py
+++ Python/2_string.py
@@ -1,1 +1,13 @@
 print("test")
+text="Anna, paweł, Jan"
+print(type(text))
+tab=text.split(',')
+print(tab)
+print(type(tab))
+name1=tab[1]
+print(name1)
+
+nameUpper=name1.upper()
+print(nameUpper)
+nameLower=name1.lower()
+print(nameLower)
